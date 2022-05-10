5032fe2668bb2ff179b826e04c51fa7d4727dd63
clipssender31 <100996857+clipssender31@users.noreply.github.com>
2022-05-10 19:51:28 +1000

    Fixed Issue

--- onliner.py
+++ onliner.py
@@ -1,25 +1,26 @@
-import websocket, threading, random, json, time, pyfiglet
+import websocket, threading, random, json, time, pyfiglet, sys
 tokens=open('Tokens.txt','r').read().splitlines()
 randomstatus=['online','idle','dnd']
 AmountOnlined=0
-
-def countdown(t):
+count=0
+def countdown(t): #this function was taken from cugeon.
     while t:
         mins, secs = divmod(t, 60)
         timer = '{:02d}:{:02d}'.format(mins, secs)
-        print(f"Time Until Re-Online: {timer}", end="\r")
+        print(f"Time Until Re-Online: {timer}", end="\r")  
         time.sleep(1)
-        time -= 1
+        t -= 1 
     print('Trying To Online Tokens')
     print(pyfiglet.figlet_format(f"Clips Token Onliner"))
-time = input("How often you'd like Re-online your tokens (500 - 600s recommended): ")
+t = input("How often you'd like Re-online your tokens (500 - 600s recommended): ")
 def websocket_token(token):
-    ws=websocket.WebSocket();ws.connect('wss://gateway.discord.gg/?encoding=json&v=9');response=ws.recv();event=json.loads(response);heartbeat_interval=event['d']['heartbeat_interval']/1000;auth={'op':2,'d':{'token':token,'capabilities':61,'properties':{'os':'Windows','browser':'Chrome','device':'','system_locale':'en-GB','browser_user_agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36','browser_version':'90.0.4430.212','os_version':'10','referrer':'','referring_domain':'','referrer_current':'','referring_domain_current':'','release_channel':'stable','client_build_number':'85108','client_event_source':'null'},'presence':{'status':random.choice(randomstatus),'since':0,'activities':[],'afk':False},'compress':False,'client_state':{'guild_hashes':{},'highest_last_message_id':'0','read_state_version':0,'user_guild_settings_version':-1}}};ws.send(json.dumps(auth))
-    while True:heartbeatJSON={'op':1,'token':token,'d':'null'};ws.send(json.dumps(heartbeatJSON));time.sleep(heartbeat_interval)
+    ws=websocket.WebSocket();ws.connect('wss://gateway.discord.gg/?encoding=json&v=9');response=ws.recv();event=json.loads(response);heartbeat_interval=event['d']['heartbeat_interval']/1000;ws.send(json.dumps({"op": 2,"d": {"token": token,"properties": {    "$os": sys.platform,    "$browser": "RTB",    "$device": f"{sys.platform} Device"},"presence": {    "game": {        "name": 'github.com/clipssender31 - Token Onliner',        "type": 0,        "details": "Join Here! https://discord.gg/wrNbhMeQeW",        "state": "https://github.com/clipssender31/Discord-Token-Onliner"    },    "status": random.choice(randomstatus),    "since": 0,    'activities':[],    "afk": False}},"s": None,"t": None}))
+    while True:
+        heartbeatJSON={'op':1,'token':token,'d':'null'};ws.send(json.dumps(heartbeatJSON));time.sleep(heartbeat_interval)
 for token in tokens:threading.Thread(target=websocket_token,args=(token,)).start();AmountOnlined+=1;print(f"{AmountOnlined} Tokens onlined")
 print(f"{AmountOnlined} Tokens Are Now Online")
 for token in tokens:
-        countdown(int(time))
+        countdown(int(t))
         AmountOnlined=0
         for token in tokens:threading.Thread(target=websocket_token,args=(token,)).start();AmountOnlined+=1;print(f"{AmountOnlined} Tokens Are Now Online")
         print(f"{AmountOnlined} Tokens Are Now Online")
